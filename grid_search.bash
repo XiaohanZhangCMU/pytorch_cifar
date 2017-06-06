46a6e8936ac2b3d452ee0d96640f05528d749672
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2017-06-06 14:17:32 -0700

    grid search possible on rice

---
+++ grid_search.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+for ((lr=40; lr <= 40; lr+=1))
+do
+    for ((bz=150; bz <= 200; bz+=10))
+    do
+	mkdir param-test-${lr}-${bz}
+	cd param-test-${lr}-${bz}
+	cp ../cnn_models.py .
+	cp ../utils.py .
+	cp ../train_cnn.py .
+	sed -i "s/BATCH_SIZE = to_be_replaced/BATCH_SIZE = ${bz}/g" train_cnn.py
+	sed -i "s/LR = to_be_replaced/LR = ${lr}/g" train_cnn.py
+
+	cat > auto_${lr}_${bz}.pbs << FIN
+#!/bin/bash
+#SBATCH --job-name=cifar10
+#SBATCH --time=5:0:0
+#SBATCH -p gpu
+#SBATCH --nodes=1
+#SBATCH --mem=60000
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=xzhang11@stanford.edu
+
+python train_cnn.py
+
+wait
+FIN
+	qsub auto_${lr}_${bz}.pbs
+	cd ..
+    done
+done
+
--- train_cnn.py
+++ train_cnn.py
@@ -3,17 +3,27 @@ import torch
 import torchvision
 import torch.nn as nn
 from torch.autograd import Variable
-from utils import progress_bar
+#from utils import progress_bar
 import cnn_models
 
-EPOCH = 1
-BATCH_SIZE = 50
-LR = 0.001    # 0.001: 43%. 0.01: 36%. 0.0001: 32%
+'''
+To do: 
+    write a dataloader for arbitrary data
+    use cnn for dislocation detection problem
+    write grid search to find optimal LR and batch size
+    separate MNIST and CIFAR10. How to organize codes?
+    utils.progressbar problem when submit jobs
+'''
+
+
+EPOCH = 5
+BATCH_SIZE = to_be_replaced #150
+LR = to_be_replaced/10000.  #0.0022
 DOWNLOAD_MNIST = False
 DOWNLOAD_CIFAR10 = False
 nnfile = 'cnn.pkl' 
 nnparamfile = 'cnn.pkl.params'
-use_cuda = False #torch.cuda.is_available()
+use_cuda = torch.cuda.is_available()
 
 def train_and_save( net, train_loader, validation_x, validation_y, lr, EPOCH, nnfile, nnparamfile):
     #SGD: %12. #ADM: 43%.
@@ -33,33 +43,37 @@ def train_and_save( net, train_loader, validation_x, validation_y, lr, EPOCH, nn
             loss.backward()
             optimizer.step()
             
-#            if batch_idx % 50 == 0: 
+            if (batch_idx+1) % 50 == 0: 
 #                test_output = net(validation_x)
 #                pred_y = torch.max(test_output,1)[1].data.squeeze()
 #                accuracy = sum(pred_y == validation_y) / validation_y.size(0)
 #                print ('Epoch: ', EPOCH, '| train loss: %.4f' % loss.data[0], '| test accuracy', accuracy)
 #
-            train_loss += loss.data[0]
-            _, predicted = torch.max(prediction.data,1)
-            total += b_y.size(0)
-            correct += predicted.eq(b_y.data).cpu().sum()
-            progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %3f%% (%d%d)'
-                    %(train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-    torch.save(net, nnfile)
-    torch.save(net.state_dict(), nnparamfile)
+                train_loss += loss.data[0]
+                _, predicted = torch.max(prediction.data,1)
+                total += b_y.size(0)
+                correct += predicted.eq(b_y.data).cpu().sum()
+                if (0) : # progress bar gives runtime error if running on nodes. Interactively fun.
+                    progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %3f%% (%d%d)'
+                        %(train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+                
+    torch.save(net, nnfile+'.'+str(LR)+'.'+str(BATCH_SIZE))
+    torch.save(net.state_dict(), nnparamfile+'.'+str(LR)+'.'+str(BATCH_SIZE))
+    f = open('Acc_'+str(LR) +'_'+ str(BATCH_SIZE)+'.txt','w')
+    f.write(str(100.*correct/total))
     return net
 
 # train to recognize MNIST data
 def main_1(argv):
     train_data = torchvision.datasets.MNIST(
-        root = './mnist',
+        root = '../mnist',
         train = True,
         transform = torchvision.transforms.ToTensor(),
         download=DOWNLOAD_MNIST
     )
     train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size=BATCH_SIZE,
                                    shuffle = True, num_workers = 2)
-    test_data = torchvision.datasets.MNIST(root='./mnist/', train=False)
+    test_data = torchvision.datasets.MNIST(root='../mnist/', train=False)
     validation_x = Variable(torch.unsqueeze(test_data.test_data, dim=1),volatile=True).type(torch.FloatTensor)[:2000]/255.
     validation_y = test_data.test_labels[:2000]
 
@@ -69,22 +83,24 @@ def main_1(argv):
 # train to reconginize CIFAR10 data
 def main(argv):
     train_data = torchvision.datasets.CIFAR10(
-        root = './cifar10',
+        root = '../cifar10',
         train = True,
-        transform = torchvision.transforms.ToTensor(),
+        transform = torchvision.transforms.Compose([ 
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]),
         download=DOWNLOAD_CIFAR10
     )
     train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size=BATCH_SIZE,
                                    shuffle = True, num_workers = 2)
-    test_data = torchvision.datasets.CIFAR10(root='./cifar10/', train=False)
+    test_data = torchvision.datasets.CIFAR10(root='../cifar10/', train=False)
     validation_x = Variable(torch.unsqueeze(torch.from_numpy(test_data.test_data), dim=1),volatile=True).type(torch.FloatTensor)[:2000]/255.
     validation_y = test_data.test_labels[:2000]
 
     net = cnn_models.CNN( )
     if use_cuda:
         net.cuda()
-        #net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count())) 
-        torch.backends.cudnn.enabled=False
+        net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count())) 
+        torch.backends.cudnn.enabled=True
         
     net = train_and_save( net, train_loader, validation_x, validation_y, LR, EPOCH, nnfile, nnparamfile)
 
